8967290188449efba66b2a1fe8e9de776e31ef84
Nikita Pieters <nikita_pieters@hotmail.com>
2023-05-18 13:31:54 +0200

    put first BobsBagels into this BobBagels

--- tdd-oop-bobs-bagels.CSharp.Source/BobsBagelsApp.cs
+++ tdd-oop-bobs-bagels.CSharp.Source/BobsBagelsApp.cs
@@ -47,6 +47,44 @@ namespace tdd_oop_bobs_bagels.CSharp.Source
 
 
 
+        public List<string> basket = new List<string>();
+        public int MaxCapacityBasket = 3;
+        public void AddProduct(string item)
+        {
+            if (this.basket.Count < this.MaxCapacityBasket)
+            {
+                basket.Add(item);
+            }
+        }
+
+        public string ChangeBasketCapacity(int count)
+        {
+            // if basket.count < count
+            if (basket.Count < count)
+            {
+                MaxCapacityBasket = count;
+                return $"The new basketsize is {count}";
+            }
+            else
+            {
+                return $"There are more items in the basket than {count}";
+            }
+            //else basket.count > count either remove items from existing basket or give back message
+        }
+
+        public string RemoveProduct(string item)
+        {
+            if (basket.Contains(item))
+            {
+                basket.Remove(item);
+                return $"{item} removed";
+            }
+            else
+            {
+                return "Item not found";
+            }
+        }
+
         private List <InventoryItem> stock = new List<InventoryItem>();
     }
 }
--- tdd-oop-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
+++ tdd-oop-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
@@ -1,12 +1,87 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tdd_oop_bobs_bagels.CSharp.Source;
 
 namespace tdd_oop_bobs_bagels.CSharp.Test
 {
-    internal class BobsBagelsTests
+    [TestFixture]
+    public class BobsBagelsTests
 {
-}
+        [Test]
+        public void InitialTest()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void TestAddBagel()
+        {
+            //I'd like to add a specific type of bagel to my basket
+            BobsBagelsApp _basket = new BobsBagelsApp();
+            _basket.AddProduct("Cheese bagel");
+            Assert.IsTrue(_basket.basket.Contains("Cheese bagel"));
+        }
+        [Test]
+        public void TestRemoveBagel()
+        {
+            //I'd like to remove a bagel from my basket
+            BobsBagelsApp _basket = new BobsBagelsApp();
+            _basket.AddProduct("Cheese bagel");
+            _basket.AddProduct("Salmon bagel");
+            _basket.RemoveProduct("Salmon bagel");
+            Assert.IsFalse(_basket.basket.Contains("Salmon bagel"));
+            Assert.AreEqual(_basket.RemoveProduct("Cheese bagel"), "Cheese bagel removed");
+
+        }
+        [Test]
+        public void TestRemoveNonExistingBagel()
+        {
+            //I'd like to know if I try to remove an item that doesn't exist in my basket.
+            BobsBagelsApp _basket = new BobsBagelsApp();
+            _basket.AddProduct("Cheese bagel");
+            _basket.RemoveProduct("Cheese bagel");
+            Assert.AreEqual(_basket.RemoveProduct("Cheese bagel"), "Item not found");
+        }
+
+        [Test]
+        public void TestBasketCapacityReached()
+        {
+            //I'd like to know when my basket is full when I try adding an item beyond my basket capacity
+            BobsBagelsApp _basket = new BobsBagelsApp();
+            _basket.AddProduct("Cheese bagel");
+            _basket.AddProduct("Salmon bagel");
+            _basket.AddProduct("Bacon bagel");
+            _basket.AddProduct("Nutella bagel");
+            Assert.AreEqual(_basket.basket.Count, _basket.MaxCapacityBasket);
+        }
+
+        [TestCase(5)]
+        public void TestChangeBasketCapacity(int count)
+        {
+            //I’d like to change the capacity of baskets
+            BobsBagelsApp _basket = new BobsBagelsApp();
+            _basket.AddProduct("Cheese bagel");
+            _basket.AddProduct("Salmon bagel");
+            _basket.AddProduct("Bacon bagel");
+            _basket.AddProduct("Nutella bagel");
+            _basket.ChangeBasketCapacity(count);
+            Assert.AreEqual(_basket.MaxCapacityBasket, 5);
+            Assert.AreEqual(_basket.ChangeBasketCapacity(count), $"The new basketsize is {count}");
+
+        }
+        [TestCase(2)]
+        public void TestChangeBasketCapacity1(int count)
+        {
+            BobsBagelsApp _basket = new BobsBagelsApp();
+            _basket.AddProduct("Cheese bagel");
+            _basket.AddProduct("Salmon bagel");
+            _basket.AddProduct("Bacon bagel");
+            _basket.AddProduct("Nutella bagel");
+            _basket.ChangeBasketCapacity(count);
+            Assert.AreEqual(_basket.ChangeBasketCapacity(count), $"There are more items in the basket than {count}");
+        }
+    }
 }
