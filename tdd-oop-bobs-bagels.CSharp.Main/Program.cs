2941ef1b043878e1ba7cb1e966baaf2db9cd7266
spiroskafiris <spiroskafiris@gmail.com>
2023-09-13 20:04:16 +0300

    Spiros Kafiris

--- tdd-oop-bobs-bagels.CSharp.Main/Program.cs
+++ tdd-oop-bobs-bagels.CSharp.Main/Program.cs
@@ -1,2 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tdd_oop_bobs_bagels.CSharp.Main;
+
+namespace tdd_bobs_bagels.CSharp.Main
+{
+    public class Core
+    {
+        public List<Object> Basket = new List<Object>();
+        public int capacity = 5;
+        private bool _isManager = false;
+
+
+        /*bool bacon = false;
+        bool egg = false;
+        bool cheese = false;
+        bool cream_cheese = false;
+        bool smoked_salmon = false;
+        bool ham = false;*/
+
+        /*Bagel OnionBagel = new Bagel("BGLO", 0.49f, "Bagel", "Onion");
+        Bagel PlainBagel = new Bagel("BGLP", 0.39f, "Bagel", "Plain");
+        Bagel EverythingBagel = new Bagel("BGLE", 0.49f, "Bagel", "Everything");
+        Bagel SesameBagel = new Bagel("BGLS", 0.49f, "Bagel", "Sesame");*/
+
+        List<bool> Fillings = new List<bool>();
+
+        public void AddFillings(bool filling)
+        {
+            Fillings.Add(filling);
+        }
+
+
+        public string AddBagel(List<Object> Basket, object Bagel)
+        {
+            this.Basket = Basket;
+            if (Basket.Count < capacity)
+            {
+                Basket.Add(Bagel);
+                return "bagel added!";
+            }
+            return "basket is full!";
+        }
+
+        public string RemoveBagel(List<Object> Basket, object Bagel)
+        {
+            this.Basket = Basket;
+            if (Basket.Contains(Bagel))
+            {
+                Basket.Remove(Bagel);
+                return "bagel removed!";
+            }
+            return "you haven't added this one!";
+        }
+
+
+        public void UpdateCapacity(List<Object> Basket, int newcap)
+        {
+            _isManager = true;
+            capacity = newcap;
+        }
+
+        public double Sum(List<object> basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- tdd-oop-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
+++ tdd-oop-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
@@ -4,16 +4,104 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tdd_bobs_bagels.CSharp.Main;
+using tdd_oop_bobs_bagels.CSharp.Main;
 
 namespace tdd_oop_bobs_bagels.CSharp.Test
 {
     [TestFixture]
-    public class BobsBagelsTests
+    public class CoreTests
     {
+        public List<bool> Fillings = new List<bool>();
+        bool bacon = false;
+        bool egg = false;
+        bool cheese = false;
+        bool cream_cheese = false;
+        bool smoked_salmon = false;
+        bool ham = false;
+
+
+        Bagel OnionBagel = new Bagel("BGLO", 0.49f, "Bagel", "Onion");
+        Bagel PlainBagel = new Bagel("BGLP", 0.39f, "Bagel", "Plain");
+        Bagel EverythingBagel = new Bagel("BGLE", 0.49f, "Bagel", "Everything");
+        Bagel SesameBagel = new Bagel("BGLS", 0.49f, "Bagel", "Sesame");
+
+
         [Test]
-        public void FirstTest()
+        public void AddBagel()
         {
-            Assert.Pass();
+            //arrange
+            Core core = new Core();
+            //act
+            core.AddBagel(core.Basket, OnionBagel);
+            core.AddBagel(core.Basket, PlainBagel);
+            core.AddBagel(core.Basket, EverythingBagel);
+            //assert
+            Assert.IsTrue(core.Basket.Contains(OnionBagel));
         }
+        [Test]
+        public void RemoveBagel()
+        {
+            //arrange
+            Core core = new Core();
+            //act
+            core.RemoveBagel(core.Basket, EverythingBagel);
+
+            //assert
+            Assert.IsFalse(core.Basket.Contains(EverythingBagel));
+        }
+
+        [Test]
+        public void UpdateCap()
+        {
+            //arrange
+            Core core = new Core();
+            int newcap = 7;
+            //act
+            core.UpdateCapacity(core.Basket, newcap);
+
+            //assert
+            Assert.IsTrue(core.capacity == newcap);
+        }
+
+        public void SumOfBasket()
+        {
+            //arrange
+            Core core = new Core();
+
+            //act
+            core.AddBagel(core.Basket, OnionBagel);
+            core.AddBagel(core.Basket, PlainBagel);
+            core.Sum(core.Basket);
+
+            //assert
+            Assert.IsTrue(core.Sum(core.Basket)==(0.49+0.39));
+        }
+
+        public void AddFillingsTest()
+        {
+            //arrange
+            Core core = new Core();
+
+            //act
+            core.AddFillings(bacon);
+            core.AddBagel(core.Basket, OnionBagel);
+            core.AddBagel(core.Basket, PlainBagel);
+
+            //assert
+            Assert.IsTrue(Fillings.Contains(bacon));
+        }
+
+        /* public void UpdateCap()
+         {
+             //arrange
+             Core core = new Core();
+
+             //act
+             core.UpdateCapacity(core.Basket, newcap);
+
+             //assert
+             Assert.IsTrue(core.Basket.Contains(bagel1));
+         }*/
     }
 }
