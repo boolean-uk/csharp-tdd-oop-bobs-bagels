d161a26a2f8ac51cac68110a8d30d7cf0dc0f10a
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-13 20:32:20 +0200

    added tests for bagels and fillings

--- tdd-oop-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
+++ tdd-oop-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using tdd_oop_bobs_bagels.CSharp.Main;
@@ -10,7 +11,9 @@ namespace tdd_oop_bobs_bagels.CSharp.Test
 {
     public class TestsForBasket
     { // deleted a lot of tests from previous bobs bagel
-        private TestsForBasket _basket;
+        
+        [TestFixture]
+        private Basket _basket;
 
         [SetUp]
         public void Setup()
@@ -33,7 +36,47 @@ namespace tdd_oop_bobs_bagels.CSharp.Test
             bool removed = _basket.RemoveItem(bagel);
             Assert.IsTrue(removed);
         }
+    }
+
+    [TestFixture]
+    public class TestsForBagels
+    {
+        private Bagel _bagel;
+        [SetUp]
+        public void Setup()
+        {
+            _bagel = new Bagel("BGLO", 0.49, "Bagel", "Onion");
+        }
+        [Test]
+        public void CanWeGetTheBagelPrice()
+        {
+            double price = -_bagel.GetPrice();
+            Assert.AreEqual(0.49, price);
+        }
+        [Test]
+        public void CanWeAddFillingToTheBagel()
+        {
+            bool succes = _bagel.AddFilling(new Filling("FILB", 0.12, "Bacon"));
+            Assert.IsTrue(succes);
+        }
+    }
+
+    [TestFixture]
+    public class TestsForFilling
+    {
+        private Filling _filling;
+        [SetUp]
+        public void Setup()
+        {
+            _filling = new Filling("FILB", 0.12, "Bacon");
+        }
+        [Test]
+        public void CanWeGetTheFillingPrice()
+        {
+            double price _filling.GetPrice();
+            Assert.AreEqual(0.12, price);
+        }
+    }
 
 
-    } 
 }
