b5eb60b80faee7bcc13ec8f3e562beff12847c64
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-19 14:27:09 +0200

    added interface and all classes

---
+++ tdd-oop-bobs-bagels.CSharp.Main/Coffee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_bobs_bagels.CSharp.Main
+{
+    internal class Coffee
+{
+}
+}
