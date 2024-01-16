88cda4a015fbfd3c1d5648f01d77378d5ee6522c
skjalg <spectraldesign1@gmail.com>
2024-01-16 11:18:55 +0100

    Test Inventory

---
+++ exercise.main/Inventory.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace exercise.main
+{
+    public class Inventory
+    {
+        public void AddItemToInventory(Bagel bagel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> GetItemsInInventory()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ exercise.tests/InventoryTest.cs
@@ -0,0 +1,26 @@
+﻿using exercise.main;
+
+namespace exercise.tests
+{
+    [TestFixture]
+    public class InventoryTest
+    {
+
+        [Test]
+        public void TestGetItemsInInventory()
+        {
+            //setup
+            Inventory inventory = new();
+            Bagel bagel = new(BagelType.Everything);
+            inventory.AddItemToInventory(bagel);
+
+            //execute
+            List<Item> items = inventory.GetItemsInInventory();
+
+            //verify
+            Assert.That(items, Is.Not.Null);
+            Assert.That(items, Has.Count.EqualTo(1));
+
+        }
+    }
+}
