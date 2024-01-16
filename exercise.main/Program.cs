fa663640ebe8d7d8659f95e232de1f754263ea7c
Gudbrand <gudbrandwd@live.no>
2024-01-16 15:50:53 +0100

    updated domain model based on changes

--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using exercise.main;
+
+UserInterface ui = new UserInterface();
+ui.run();
---
+++ exercise.main/UserInterface.cs
@@ -0,0 +1,28 @@
+﻿namespace exercise.main;
+
+public class UserInterface
+{
+    public void run()
+    {
+        bool running = true;
+        while (running)
+        {
+            Console.Clear();
+            printMenu();
+            string selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "0":
+                    running = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void printMenu()
+    {
+        Console.WriteLine("0. Exit");
+    }
+}
