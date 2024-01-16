51dd330590b33ed48527def37c1c94db74de0a31
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-01-16 14:09:24 +0100

    Added in sourcecode and tests from last Bobs Bagels

---
+++ exercise.main/Core.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.main
+{
+
+    public class Core
+    {
+        private List<string> _bagels;
+        int _nrBagels;
+        int _capacity;
+
+        public Core()
+        {
+            _bagels = new List<string>();
+            _nrBagels = 0;
+            _capacity = 5;
+        }
+
+        public List<string> Bagels { get { return _bagels; } }
+
+        public void Add(string bagel)
+        {
+            if (_nrBagels != _capacity)
+            {
+                Bagels.Add(bagel);
+                _nrBagels++;
+            }
+            else
+            {
+                throw new Exception("Basket is full");
+            }
+        }
+
+        public void Remove(string bagel)
+        {
+            if (!Bagels.Remove(bagel))
+            {
+                throw new Exception("Bagel is not in basket");
+            }
+        }
+
+        public void IncreaseCapacity()
+        {
+
+            //BIG customer waves for breakfast :PPPP
+            _capacity *= 3; //upping capacity 3 times.
+            Console.WriteLine($"Increased capacity to: {_capacity}");
+        }
+    }
+}
--- exercise.tests/UnitTest1.cs
+++ exercise.tests/UnitTest1.cs
@@ -1,15 +1,100 @@
+using exercise.main;
+using System.Threading.Tasks.Sources;
+using NUnit.Framework;
+
+
 namespace exercise.tests;
 
-public class Tests
+[TestFixture]
+public class BobsBagelsTests
 {
-    [SetUp]
-    public void Setup()
+
+    [Test]
+    public void TestRun()
     {
+
+    }
+    public void TestAdd()
+    {
+        Core core = new Core();
+
+        core.Add("Plain");
+
+        Assert.That(core.Bagels.Count, Is.EqualTo(1));
+        Assert.That(core.Bagels[0], Is.EqualTo("Plain"));
     }
 
     [Test]
-    public void Test1()
+    public void TestAddFull()
     {
-        Assert.Pass();
+        Core core = new Core();
+
+        core.Add("Nutella");
+        core.Add("Gluten-free");
+        core.Add("Bacon");
+        core.Add("Cheese");
+        core.Add("Egg");
+
+        Assert.Throws<Exception>(() => core.Add("Cheddar"));
+        foreach (var bagel in core.Bagels)
+        {
+            Console.WriteLine(bagel);
+        }
+
+    }
+
+    [Test]
+    public void TestRemove()
+    {
+        Core core = new Core();
+
+        core.Add("Plain");
+        core.Remove("Plain");
+
+        Assert.That(core.Bagels.Count, Is.EqualTo(0), "Bagel count should be 0");
+        Assert.IsFalse(core.Bagels.Contains("Plain"), "Bagel 'Plain' should be removed"); //checks ifFalse on bagel is in List<>
+
+        Console.WriteLine("Bagels in the list after removal:");
+        foreach (var bagel in core.Bagels)
+        {
+            Console.WriteLine(bagel); //outputs nothing since nothing is in list
+        }
+
+
+    }
+
+    [Test]
+    public void TestRemoveNonexistant()
+    {
+        Core core = new Core();
+
+        core.Add("Plain");
+
+        Assert.Throws<Exception>(() => core.Remove("Cheddar"));
+        Console.WriteLine(core.Equals(core));
+    }
+
+    [Test]
+    public void UpCapacity()
+    {
+        Core core = new Core();
+
+        core.Add("Nutella");
+        core.Add("Gluten-free");
+        core.Add("Bacon");
+        core.Add("Cheese");
+        core.Add("Egg");
+
+        core.IncreaseCapacity();
+        core.Add("Cheddar");
+        core.Add("Anette");
+        core.Add("Mari");
+
+        Assert.That(core.Bagels.Count, Is.EqualTo(8));
+        Console.WriteLine($"Bagels count after capacity increase: {core.Bagels.Count}");
+        foreach (var bagel in core.Bagels)
+        {
+            Console.WriteLine(bagel);
+        }
     }
 }
