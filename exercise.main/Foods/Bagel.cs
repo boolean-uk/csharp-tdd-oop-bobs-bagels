79e3ae6bdb613489c9c6540e6f0e287fe3ac58d2
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-01-16 10:26:00 +0100

    Add filling to bagel

--- exercise.main/Foods/Bagel.cs
+++ exercise.main/Foods/Bagel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace exercise.main.Foods
 {
-    public class Bagel(BagelVariant variant) : Food<BagelVariant>(variant)
+    public class Bagel: Food<BagelVariant>
     {
         public override string Name => "Bagel";
         public override string Sku
@@ -17,5 +17,14 @@ namespace exercise.main.Foods
                 return "BGL" + Variant.ToString().ToUpper()[..1];
             }
         }
+        public Filling Filling { get; set; }
+
+        public Bagel(BagelVariant variant) : base(variant) { }
+
+        public Bagel(BagelVariant variant, Filling filling) : base(variant) 
+        {
+            Filling = filling;
+        }
+
     }
 }
