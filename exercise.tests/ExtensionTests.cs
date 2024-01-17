d537f9c4d9c37f9e6df3681d2cdf906756af48c0
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-17 14:11:39 +0100

    Updated test values due to calculation errors when writing

--- exercise.tests/ExtensionTests.cs
+++ exercise.tests/ExtensionTests.cs
@@ -12,11 +12,12 @@ namespace exercise.tests
     [TestFixture]
     public class ExtensionTests
     {
+        private float _tolerance = 0.0001f;
+
         [Test]
         public void DiscountBulkBagelsTest()
         {
             // Arrange
-            DiscountManager discounter = new DiscountManager();
             Basket basket1 = new Basket(); // 6 bagels
             Basket basket2 = new Basket(); // 12 bagels
             Basket basket3 = new Basket(); // 7 bagels
@@ -56,22 +57,22 @@ namespace exercise.tests
 
             // Act
             List<Discount> discounts1 = new List<Discount>();
-            float res1 = discounter.ApplyDiscounts(basket1, out discounts1);
+            float res1 = DiscountManager.ApplyDiscounts(basket1, out discounts1);
             List<Discount> discounts2 = new List<Discount>();
-            float res2 = discounter.ApplyDiscounts(basket2, out discounts2);
+            float res2 = DiscountManager.ApplyDiscounts(basket2, out discounts2);
             List<Discount> discounts3 = new List<Discount>();
-            float res3 = discounter.ApplyDiscounts(basket3, out discounts3);
+            float res3 = DiscountManager.ApplyDiscounts(basket3, out discounts3);
 
             // Assert
-            Assert.That(res1, Is.EqualTo(3.21f));
+            Assert.That(res1, Is.EqualTo(3.21f).Within(_tolerance));
             Assert.That(discounts1[0].Name, Is.EqualTo("6 bagels"));
             Assert.That(discounts1[0].DiscountPrice, Is.EqualTo(2.49f));
 
-            Assert.That(res2, Is.EqualTo(5.43f));
+            Assert.That(res2, Is.EqualTo(5.43f).Within(_tolerance));
             Assert.That(discounts2[0].Name, Is.EqualTo("12 bagels"));
             Assert.That(discounts2[0].DiscountPrice, Is.EqualTo(3.99f));
 
-            Assert.That(res3, Is.EqualTo(3.72f));
+            Assert.That(res3, Is.EqualTo(3.72f).Within(_tolerance));
             Assert.That(discounts3[0].Name, Is.EqualTo("6 bagels"));
             Assert.That(discounts3[0].DiscountPrice, Is.EqualTo(2.49f));
         }
@@ -80,7 +81,6 @@ namespace exercise.tests
         public void DiscountBulkBagelAndCoffeeTest()
         {
             // Arrange
-            DiscountManager discounter = new DiscountManager();
             Basket basket1 = new Basket(); // 6 bagels
             Basket basket2 = new Basket(); // 7 bagels 1 coffee
             Basket basket3 = new Basket(); // 1 bagel 1 coffee
@@ -107,31 +107,32 @@ namespace exercise.tests
             basket2.AddItemToBasket(prod1);
             basket2.AddItemToBasket(prod2);
 
+            // Bagel and coffee discount
             basket3.AddItemToBasket(prod1);
             basket3.AddItemToBasket(prod2);
 
             // Act
             List<Discount> discounts1 = new List<Discount>();
-            float res1 = discounter.ApplyDiscounts(basket1, out discounts1);
+            float totalPrice1 = DiscountManager.ApplyDiscounts(basket1, out discounts1);
             List<Discount> discounts2 = new List<Discount>();
-            float res2 = discounter.ApplyDiscounts(basket2, out discounts2);
+            float totalPrice2 = DiscountManager.ApplyDiscounts(basket2, out discounts2);
             List<Discount> discounts3 = new List<Discount>();
-            float res3 = discounter.ApplyDiscounts(basket3, out discounts3);
+            float totalPrice3 = DiscountManager.ApplyDiscounts(basket3, out discounts3);
 
             // Assert
-            Assert.That(res1, Is.EqualTo(3.21f));
+            Assert.That(totalPrice1, Is.EqualTo(4.2f).Within(_tolerance));
             Assert.That(discounts1[0].Name, Is.EqualTo("6 bagels"));
-            Assert.That(discounts1[0].DiscountPrice, Is.EqualTo(2.49f));
+            Assert.That(discounts1[0].DiscountPrice, Is.EqualTo(2.49f).Within(_tolerance));
 
-            Assert.That(res2, Is.EqualTo(5.43f));
+            Assert.That(totalPrice2, Is.EqualTo(4.58f).Within(_tolerance));
             Assert.That(discounts2[0].Name, Is.EqualTo("6 bagels"));
-            Assert.That(discounts2[0].DiscountPrice, Is.EqualTo(2.49f));
+            Assert.That(discounts2[0].DiscountPrice, Is.EqualTo(2.49f).Within(_tolerance));
             Assert.That(discounts2[1].Name, Is.EqualTo("coffee and bagel"));
-            Assert.That(discounts2[1].DiscountPrice, Is.EqualTo(1.25f));
+            Assert.That(discounts2[1].DiscountPrice, Is.EqualTo(1.25f).Within(_tolerance));
 
-            Assert.That(res3, Is.EqualTo(5.43f));
+            Assert.That(totalPrice3, Is.EqualTo(1.37f).Within(_tolerance));
             Assert.That(discounts3[0].Name, Is.EqualTo("coffee and bagel"));
-            Assert.That(discounts3[0].DiscountPrice, Is.EqualTo(1.25f));
+            Assert.That(discounts3[0].DiscountPrice, Is.EqualTo(1.25f).Within(_tolerance));
         }
 
 
@@ -139,7 +140,6 @@ namespace exercise.tests
     public void DiscountDoubleBulkBagelsTest()
     {
         // Arrange
-        DiscountManager discounter = new DiscountManager();
         Basket basket = new Basket(); // 12 bagels
         basket.SetBasketSize(30);
         Product prod1 = ProductFactory.GenerateProduct(new string[] { "BGLO", "FILE" });
@@ -175,15 +175,15 @@ namespace exercise.tests
 
         // Act
         List<Discount> discounts = new List<Discount>();
-        float res = discounter.ApplyDiscounts(basket, out discounts);
+        float res = DiscountManager.ApplyDiscounts(basket, out discounts);
 
 
         // Assert
-        Assert.That(res, Is.EqualTo(10.86f));
+        Assert.That(res, Is.EqualTo(10.86f).Within(_tolerance));
         Assert.That(discounts[0].Name, Is.EqualTo("12 bagels"));
-        Assert.That(discounts[0].DiscountPrice, Is.EqualTo(3.99f));
+        Assert.That(discounts[0].DiscountPrice, Is.EqualTo(3.99f).Within(_tolerance));
         Assert.That(discounts[1].Name, Is.EqualTo("12 bagels"));
-        Assert.That(discounts[1].DiscountPrice, Is.EqualTo(3.99f));
+        Assert.That(discounts[1].DiscountPrice, Is.EqualTo(3.99f).Within(_tolerance));
 
         }
     }
