7385a70aebcc02ff02dda17353df659e0329ad4e
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-01-17 10:15:22 +0100

    Make summaries for methods

--- exercise.main/Foods/IFood.cs
+++ exercise.main/Foods/IFood.cs
@@ -8,9 +8,21 @@ namespace exercise.main.Foods
 {
     public interface IFood
     {
+        /// <summary>
+        /// Gets the name of the type of food (E.g. Bagel, Coffee, Filling)
+        /// </summary>
         string Name { get; }
+        /// <summary>
+        /// Gets the SKU of the food
+        /// </summary>
         string Sku { get; }
+        /// <summary>
+        /// Gets the price of the food
+        /// </summary>
         float Price { get; }
+        /// <summary>
+        /// Gets the name concatenated with the variant of the food
+        /// </summary>
         string FullName { get; }
     }
 }
--- exercise.main/Receipt.cs
+++ exercise.main/Receipt.cs
@@ -9,16 +9,22 @@
             string date = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
             string border = new('-', title.Length * 2);
             var foodGroups = content.GroupBy(x => x.FullName)
-                .Select(group => new { Name = group.Key, Count = group.Count(), Price = basket.GetTotalPrice(group.ToList()), Discount = basket.CalculateDiscounts(group.ToList()) });
+                .Select(group => new
+                {
+                    Name = group.Key,
+                    Count = group.Count(),
+                    Price = basket.GetTotalPrice(group.ToList()),
+                    Discount = basket.CalculateDiscounts(group.ToList())
+                });
 
             Console.WriteLine(
-                $"\t{title,10}\n\n" +
-                $"\t{date,10}\n\n" +
-                $"{border,10}\n");
+                $"\t{title, 10}\n\n" +
+                $"\t{date, 10}\n\n" +
+                $"{border, 10}\n");
             foreach (var group in foodGroups)
             {
                 Console.WriteLine(
-                    $"{group.Name,-10}{group.Count,10}{group.Price,18:0.00}$");
+                    $"{group.Name, -10}{group.Count, 10}{group.Price, 18:0.00}$");
                 if (group.Discount > 0)
                 {
                     Console.Write("\t{0, 35}", $"-({group.Discount:0.00}$)\n");
@@ -27,9 +33,9 @@
             Console.WriteLine(
                 $"{border}\n" +
                 $"Total:" +
-                $"\t{basket.GetTotalPrice(),30:0.00}$");
+                $"\t{basket.GetTotalPrice(), 30:0.00}$");
             Console.WriteLine("{0,25}\n{1,25}", $"You saved {basket.CalculateDiscounts(basket.GetContents()):0.00}$", "on this purchase.");
-            Console.WriteLine($"\n\t{"Thank you\n",16}" + $"\t{"for your order!",18}");
+            Console.WriteLine($"\n\t{"Thank you\n", 16}" + $"\t{"for your order!", 18}");
         }
     }
 }
