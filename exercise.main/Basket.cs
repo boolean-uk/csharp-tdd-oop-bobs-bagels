bc997a91e24b503bc3254f3a544f823b611dbc63
Gudbrand <gudbrandwd@live.no>
2024-01-18 11:09:04 +0100

    got discounts working

--- exercise.main/Basket.cs
+++ exercise.main/Basket.cs
@@ -77,18 +77,34 @@ public class Basket
         double price = 0.00;
         var bagels = _products.OfType<Bagel>().ToList();
         var coffees = _products.OfType<Product>().ToList();
+        int packsOf12 = calculate12Packs(bagels.Count);
+        bool packOf6 = calculate6Pack(bagels.Count);
+        int numberOfFreeBagels = 12 * packsOf12;
+        if (packOf6) numberOfFreeBagels += 6;
+        int coffeeDeals = calculateCoffeDeals(bagels.Count - numberOfFreeBagels, coffees.Count);
+        numberOfFreeBagels += coffeeDeals;
+        int numberOfFreeCoffees = coffeeDeals;
         foreach (var item in coffees)
         {
-            price += _menu.getItem(item.Sku()).Item1;
+            if (numberOfFreeCoffees > 0)
+                numberOfFreeCoffees--;
+            else
+                price += _menu.getItem(item.Sku()).Item1;
         }
         foreach (var item in bagels)
         {
-            price += _menu.getItem(item.Sku()).Item1;
+            if (numberOfFreeBagels > 0)
+                numberOfFreeBagels--;
+            else 
+                price += _menu.getItem(item.Sku()).Item1;
             if (item.Filling != null)
             {
                 price += _menu.getItem(item.Filling.Sku()).Item1;
             }
         }
+        if (packsOf12 > 0) price += packsOf12 * 3.99;
+        if (packOf6) price += 2.49;
+        if (coffeeDeals > 0) price += coffeeDeals * 1.25;
         return price;
     }
 
@@ -104,26 +120,85 @@ public class Basket
         {
             var bagels = _products.OfType<Bagel>().ToList();
             var coffees = _products.OfType<Product>().ToList();
+            int packsOf12 = calculate12Packs(bagels.Count);
+            bool packOf6 = calculate6Pack(bagels.Count);
+            int numberOfFreeBagels = 12 * packsOf12;
+            if (packOf6) numberOfFreeBagels += 6;
+            int coffeeDeals = calculateCoffeDeals(bagels.Count - numberOfFreeBagels, coffees.Count);
+            numberOfFreeBagels += coffeeDeals;
+            int numberOfFreeCoffees = coffeeDeals;
             foreach (var item in bagels)
             {
                 var bagelData = _menu.getItem(item.Sku());
+
                 if (item.Filling != null)
                 {
                     var fillingData = _menu.getItem(item.Filling.Sku());
-                    Console.WriteLine($"Id: {item.Id()} Product: {bagelData.Item2} {bagelData.Item3} Filling: {fillingData.Item3} price: {bagelData.Item1 + fillingData.Item1}");
+                    if (numberOfFreeBagels > 0)
+                    {
+                        Console.WriteLine($"Id: {item.Id()} Product: {bagelData.Item2} {bagelData.Item3} Filling: {fillingData.Item3} price: {fillingData.Item1}");
+                        numberOfFreeBagels--;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Id: {item.Id()} Product: {bagelData.Item2} {bagelData.Item3} Filling: {fillingData.Item3} price: {bagelData.Item1 + fillingData.Item1}");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"Id: {item.Id()} Product: {bagelData.Item2} {bagelData.Item3} price: {bagelData.Item1}");
+                    if (numberOfFreeBagels > 0)
+                    {
+                        Console.WriteLine($"Id: {item.Id()} Product: {bagelData.Item2} {bagelData.Item3} price: 0");
+                        numberOfFreeBagels--;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Id: {item.Id()} Product: {bagelData.Item2} {bagelData.Item3} price: {bagelData.Item1}");
+                    }
                 }
             }
             foreach(var item in coffees)
             {
                 var coffeeData = _menu.getItem(item.Sku());
-                Console.WriteLine($"Id: {item.Id()} Product: {coffeeData.Item2} {coffeeData.Item3} price: {coffeeData.Item1}");
+                if(numberOfFreeCoffees > 0)
+                {
+                    Console.WriteLine($"Id: {item.Id()} Product: {coffeeData.Item2} {coffeeData.Item3} price: 0");
+                    numberOfFreeCoffees--;
+                }
+                else
+                {
+                    Console.WriteLine($"Id: {item.Id()} Product: {coffeeData.Item2} {coffeeData.Item3} price: {coffeeData.Item1}");
+                }
             }
+            if (packsOf12 > 0) Console.WriteLine($"Discount {packsOf12} packs of 12 bagels totalling {packsOf12 * 3.99} (filling not included in price)");
+
+            if (packOf6) Console.WriteLine("Discount 6 pack of bagels for 2.49 (filling not included in price)");
+
+            if (coffeeDeals > 0) Console.WriteLine($"Discount {coffeeDeals} deals of one coffe and bagel totalling {coffeeDeals * 1.25} (filling not included in price)");
+
             Console.WriteLine($"Total price: {getBasketPrice()}");
         }
         Console.WriteLine("----------------------");
+    }
+
+    public int calculate12Packs(int bagels)
+    {
+        int rest = bagels % 12;
+        int numberOfPacks = (bagels - rest) / 12;
+        return numberOfPacks;
+    }
+
+    public bool calculate6Pack(int bagels)
+    {
+        int bagelsLeftAfter12Packs = bagels % 12;
+        if (bagelsLeftAfter12Packs >= 6) return true;
+        return false;
+    }
+
+    public int calculateCoffeDeals(int bagels, int coffees)
+    {
+        if(bagels <= coffees)
+            return bagels;
+        return coffees;
     }
 }
--- exercise.tests/BasketTests.cs
+++ exercise.tests/BasketTests.cs
@@ -70,20 +70,72 @@ public class BasketTests
         Assert.That(result, Is.EqualTo(expectedPrice));
     }
 
-    [Test]
-    public void getBasketPriceTest()
+    [TestCase(0, 0, 0)]
+    [TestCase(1, 0, 0.49)]
+    [TestCase(0, 1, 0.99)]
+    [TestCase(1, 1, 1.25)]
+    [TestCase(6, 0, 2.49)]
+    [TestCase(7, 1, 2.49 + 1.25)]
+    [TestCase(12, 0, 3.99)]
+    [TestCase(13, 1, 3.99 + 1.25)]
+    [TestCase(18, 0, 3.99 + 2.49)]
+    [TestCase(19, 1, 3.99 + 2.49 + 1.25)]
+    public void getBasketPriceTest(int bagels, int coffees, double shouldBe)
     {
         Basket basket = new Basket();
 
+        basket.setCapacity(100);
+        for (int i = 0; i < bagels; i++)
+        {
+            basket.add(i, "BGLO");
+        }
+        for (int i = 0; i < coffees; i++)
+        {
+            basket.add(i + bagels, "COFB");
+        }
         double result = basket.getBasketPrice();
-        basket.add(1, "BGLP");
-        basket.addFilling(1, "FILE");
-        double result1 = basket.getBasketPrice();
-        basket.add(2, "COFB");
-        double result2 = basket.getBasketPrice();
 
-        Assert.That(result, Is.EqualTo(0.00));
-        Assert.That(result1, Is.EqualTo(0.51));
-        Assert.That(result2, Is.EqualTo(1.50));
+        Assert.That(result, Is.EqualTo(shouldBe));
+    }
+
+    [TestCase(0, 0)]
+    [TestCase(11, 0)]
+    [TestCase(12, 1)]
+    [TestCase(13, 1)]
+    [TestCase(25, 2)]
+    public void calculate12PacksTest(int bagels, int shoudBe)
+    {
+        Basket basket = new Basket();
+
+        int result = basket.calculate12Packs(bagels);
+
+        Assert.That(result, Is.EqualTo(shoudBe));
+    }
+
+    [TestCase(0, false)]
+    [TestCase(11, true)]
+    [TestCase(12, false)]
+    [TestCase(18, true)]
+    [TestCase(25, false)]
+    public void calculate6PackTest(int bagels, bool shoudBe)
+    {
+        Basket basket = new Basket();
+
+        bool result = basket.calculate6Pack(bagels);
+
+        Assert.That(result, Is.EqualTo(shoudBe));
+    }
+
+    [TestCase(0, 0, 0)]
+    [TestCase(11, 0, 0)]
+    [TestCase(0, 5, 0)]
+    [TestCase(13, 3, 3)]
+    public void calculateCoffeDealsTest(int bagels, int coffees, int shoudBe)
+    {
+        Basket basket = new Basket();
+
+        int result = basket.calculateCoffeDeals(bagels, coffees);
+
+        Assert.That(result, Is.EqualTo(shoudBe));
     }
 }
