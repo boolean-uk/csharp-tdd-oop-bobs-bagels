1151dae925a87d6feb46750afbb6eaae41f6a251
Mikolaj <mkmbaran@gmail.com>
2024-01-21 14:57:38 +0100

    Designed a domain model for the user stories, created tests and source code that will succesfully complete tests, core exercise complete and extension 2

--- exercise.main/Basket.cs
+++ exercise.main/Basket.cs
@@ -8,11 +8,27 @@ namespace exercise.main
 {
     public class Basket
     {
-        //public List<String> myBasket = new List<String>();
-
         public List<Item> myBasket = new List<Item>();
 
-        public int BasketSize { get; set; } = 5;
+        public int BasketSize { get; set; } = 5; // default basket capacity
+
+        public List<InventoryItem> inventory = new List<InventoryItem>
+        {
+            new InventoryItem { Product = "BGLO", Price = 0.49, Name = "Bagel", Variant = "Onion" },
+            new InventoryItem { Product = "BGLP", Price = 0.39, Name = "Bagel", Variant = "Plain" },
+            new InventoryItem { Product = "BGLE", Price = 0.49, Name = "Bagel", Variant = "Everything" },
+            new InventoryItem { Product = "BGLS", Price = 0.49, Name = "Bagel", Variant = "Sesame" },
+            new InventoryItem { Product = "COFB", Price = 0.99, Name = "Coffee", Variant = "Black" },
+            new InventoryItem { Product = "COFW", Price = 1.19, Name = "Coffee", Variant = "White" },
+            new InventoryItem { Product = "COFC", Price = 1.29, Name = "Coffee", Variant = "Capuccino" },
+            new InventoryItem { Product = "COFL", Price = 1.29, Name = "Coffee", Variant = "Latte" },
+            new InventoryItem { Product = "FILB", Price = 0.12, Name = "Filling", Variant = "Bacon" },
+            new InventoryItem { Product = "FILE", Price = 0.12, Name = "Filling", Variant = "Egg" },
+            new InventoryItem { Product = "FILC", Price = 0.12, Name = "Filling", Variant = "Cheese" },
+            new InventoryItem { Product = "FILX", Price = 0.12, Name = "Filling", Variant = "Cream Cheese" },
+            new InventoryItem { Product = "FILS", Price = 0.12, Name = "Filling", Variant = "Smoked Salmon" },
+            new InventoryItem { Product = "FILH", Price = 0.12, Name = "Filling", Variant = "Ham" }
+        };
 
         public bool AddItem(Item item)
         {
@@ -60,6 +76,89 @@ namespace exercise.main
                 Console.WriteLine($"{item} exists and can be removed from basket");
             }
             return hypotheticalItem;
+        }
+
+        public double Total(List<Item> items)
+        {
+            double sum = 0;
+            foreach (var item in items)
+            {
+                sum = sum + item.Price;
+            }
+            return sum;
+        }
+
+        public double Price(string item)
+        {
+            double sum = 0;
+            var prices = new Dictionary<string, double>
+            {
+                { "Onion", 0.49 },
+                { "Plain", 0.39 },
+                { "Everything", 0.49 },
+                { "Sesame", 0.49 },
+                { "Black", 0.99 },
+                { "White", 1.19 },
+                { "Cappuccino", 1.29 },
+                { "Latte", 1.29 },
+                { "Bacon", 0.12 },
+                { "Egg", 0.12 },
+                { "Cheese", 0.12 },
+                { "Cream Cheese", 0.12 },
+                { "Ham", 0.12 }
+            };
+            
+            if (prices.ContainsKey(item))
+            {
+                sum = prices[item];
+            }
+            return sum;
+        }
+        public Item ChangeFilling(Item filling, string product, double price, string name, string variant)
+        {
+            filling.Product = product;
+            filling.Price = price;
+            filling.Name = name;
+            filling.Variant = variant;
+
+            return filling;
+        }
+
+        public Boolean InInventory(Item item, List<InventoryItem> inventory)
+        {
+            bool isStocked = false;
+            if (inventory.Any(x => x.Product == item.Product) && inventory.Any(x => x.Variant == item.Variant))
+            {
+                isStocked = true;
+            }
+            else { isStocked = false; }
+
+            return isStocked;
+        }
+
+        // Extension 2
+        public double Receipt(List<Item> myBasket)
+        {
+            Console.WriteLine($"          ~~ Bob's Bagels ~~");
+            Console.WriteLine();
+            Console.WriteLine($"          {DateTime.Now.ToString()}");
+            Console.WriteLine();
+            Console.WriteLine("{0,10}      {1,10}      {2,10}",
+                 "Product", "Qty", "Price");
+            foreach (Item item in myBasket)
+            {
+                Console.WriteLine("{0,10}      {1,10}      {2,10}",
+                    item.Variant, 1, 
+                    $"€{item.Price}"
+                    );
+            }
+            Console.WriteLine(new string('-', 40));
+            double receiptSum = 0;
+            myBasket.ForEach(b => {  receiptSum += b.Price; 
+            });
+            Console.WriteLine($"Total:                               €{receiptSum}");
+
+            return receiptSum;
         }
     }
 }
--- exercise.tests/BasketTest.cs
+++ exercise.tests/BasketTest.cs
@@ -10,8 +10,8 @@ public class BasketTest
     {
     }
 
-    [Test]
-    public void AddTest()
+    [Test] // Core exercise
+    public void AddTest() // User story 1
     {
         //arrange
         Basket basket = new Basket();
@@ -23,11 +23,11 @@ public class BasketTest
         basket.AddItem(coffee);
         basket.AddItem(filling);
         //assert
-        Assert.AreEqual(3, basket.myBasket.Count);
+        Assert.That(basket.myBasket.Count, Is.EqualTo(3));
         Assert.IsTrue(basket.myBasket.Count <= basket.BasketSize);
     }
     [Test]
-    public void RemoveTest()
+    public void RemoveTest() // User story 2
     {
         //arrange
         Basket basket = new Basket();
@@ -41,11 +41,11 @@ public class BasketTest
         basket.RemoveItem(coffee);
         basket.RemoveItem(filling);
         //assert
-        Assert.AreEqual(1, basket.myBasket.Count);
+        Assert.That(basket.myBasket.Count, Is.EqualTo(1));
         Assert.IsTrue(basket.myBasket.Count <= basket.BasketSize);
     }
     [Test]
-    public void FullBasketTest()
+    public void FullBasketTest() // User story 3
     {
         //arrange
         Basket basket = new Basket();
@@ -65,7 +65,7 @@ public class BasketTest
         Assert.IsTrue(isFull);
     }
     [Test]
-    public void BasketCapacityTest()
+    public void BasketCapacityTest() // User story 4
     {
         //arrange
         Basket basket = new Basket();
@@ -84,11 +84,11 @@ public class BasketTest
         basket.BasketCapacity(6);
         basket.AddItem(coffee2);
         //assert
-        Assert.AreEqual(6, basket.myBasket.Count);
+        Assert.That(basket.myBasket.Count, Is.EqualTo(6));
         Assert.IsTrue(basket.myBasket.Count <= basket.BasketSize);
     }
     [Test]
-    public void SchrodingersBagelTest()
+    public void SchrodingersBagelTest() // User story 5
     {
         //arrange
         Basket basket = new Basket();
@@ -109,5 +109,108 @@ public class BasketTest
         //assert
         Assert.IsTrue(exists1);
         Assert.IsFalse(exists2);
+    }
+    [Test]
+    public void TotalTest() // User story 6
+    {
+        //arrange
+        Basket basket = new Basket();
+        Item bagel = new Item("BGLO", 0.49, "Bagel", "Onion");
+        Item bagel2 = new Item("BGLS", 0.49, "Bagel", "Sesame");
+        Item coffee = new Item("COFC", 1.19, "Coffee", "Cappucino");
+        Item filling = new Item("FILB", 0.12, "Filling", "Bacon");
+        Item filling2 = new Item("FILE", 0.12, "Filling", "Egg");
+        basket.AddItem(bagel);
+        basket.AddItem(bagel2);
+        basket.AddItem(coffee);
+        basket.AddItem(filling);
+        basket.AddItem(filling2);
+        //act
+        double sum = basket.Total(basket.myBasket);
+        //assert
+        //0.49 + 0.49 + 1.19 + 0.12 + 0.12 = 2.41
+        Assert.That(sum, Is.EqualTo(2.41d));
+    }
+    [Test]
+    public void PriceTest() // User story 7 & 9
+    {
+        // using the same test for user story 7 and 9 since they are both
+        // about returning a Price from string Name on select Item
+
+        //arrange
+        Basket basket = new Basket();
+
+        //act
+        double price1 = basket.Price("Everything");
+        double price2 = basket.Price("Latte");
+        double price3 = basket.Price("Cheese");
+        //assert
+        Assert.That(price1, Is.EqualTo(0.49));
+        Assert.That(price2, Is.EqualTo(1.29));
+        Assert.That(price3, Is.EqualTo(0.12));
+    }
+    [Test]
+    public void ChangeFillingTest() // User story 8
+    {
+        //arrange
+        Basket basket = new Basket();
+        Item bagel = new Item("BGLO", 0.49, "Bagel", "Onion");
+        Item bagel2 = new Item("BGLS", 0.49, "Bagel", "Sesame");
+        Item coffee = new Item("COFC", 1.19, "Coffee", "Cappucino");
+        Item filling = new Item("FILB", 0.12, "Filling", "Bacon");
+        Item filling2 = new Item("FILE", 0.12, "Filling", "Egg");
+        basket.AddItem(bagel);
+        basket.AddItem(bagel2);
+        basket.AddItem(coffee);
+        basket.AddItem(filling);
+        //act
+        basket.RemoveItem(filling);
+        Item newfilling = basket.ChangeFilling(filling, "FILE", 0.12, "Filling", "Egg");
+        basket.AddItem(newfilling);
+        //assert
+        Assert.That(newfilling.Product, Is.EqualTo(filling2.Product));
+    }
+    [Test]
+    public void inInventoryTest() // User story 10
+    {
+        //arrange
+        Basket basket = new Basket();
+        Item bagel = new Item("BGLO", 0.49, "Bagel", "Onion");
+        Item bagel2 = new Item("BGLS", 0.49, "Bagel", "Sesame");
+        Item coffee = new Item("COFC", 1.19, "Coffee", "Capuccino");
+        Item filling = new Item("FILB", 0.12, "Filling", "Bacon");
+        Item filling2 = new Item("FILE", 0.12, "Filling", "Egg");
+        //act
+        bool bagelStocked = basket.InInventory(bagel, basket.inventory);
+        bool bagel2Stocked = basket.InInventory(bagel2, basket.inventory);
+        bool coffeeStocked = basket.InInventory(coffee, basket.inventory);
+        bool fillingStocked = basket.InInventory(filling, basket.inventory);
+        bool filling2Stocked = basket.InInventory(filling2, basket.inventory);
+        //assert
+        Assert.IsTrue(bagelStocked);
+        Assert.IsTrue(bagel2Stocked);
+        Assert.IsTrue(coffeeStocked); 
+        Assert.IsTrue(fillingStocked);
+        Assert.IsTrue(filling2Stocked);
+    }
+    [Test]
+    public void ReceiptTest() // Extension 2
+    {
+        //arrange
+        Basket basket = new Basket();
+        Item bagel = new Item("BGLO", 0.49, "Bagel", "Onion");
+        Item bagel2 = new Item("BGLS", 0.49, "Bagel", "Sesame");
+        Item coffee = new Item("COFC", 1.19, "Coffee", "Capuccino");
+        Item filling = new Item("FILB", 0.12, "Filling", "Bacon");
+        Item filling2 = new Item("FILE", 0.12, "Filling", "Egg");
+        basket.AddItem(bagel);
+        basket.AddItem(bagel2);
+        basket.AddItem(coffee);
+        basket.AddItem(filling);
+        basket.AddItem(filling2);
+        //act
+        double receiptSum = basket.Receipt(basket.myBasket);
+        //assert
+        Assert.AreEqual(2.41, receiptSum); // total sum on receipt should be €2.41
     }
 }
