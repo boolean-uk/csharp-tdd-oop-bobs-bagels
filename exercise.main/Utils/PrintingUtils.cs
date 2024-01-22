3f84eeeeb521f5c41d638da5655f12dc7104b394
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-22 10:09:25 +0100

    Resolved a bug in applying coffee discount

--- exercise.main/Utils/PrintingUtils.cs
+++ exercise.main/Utils/PrintingUtils.cs
@@ -21,7 +21,7 @@ namespace exercise.main.Utils
             List<string> SKUs = Inventory.GetValidProductSKUs();
             List<IProduct> sortedProducts = products.OrderBy(p => p.GetBasePrice()).ToList();
             List<IProduct> pickListOfProducts = new List<IProduct>(sortedProducts); // Provided to each discount application by reference
-            Dictionary<string, float> SKUDiscount = new Dictionary<string, float>(); // Provided to each discount application by reference
+            Dictionary<string, Tuple<float, bool>> SKUDiscount = new Dictionary<string, Tuple<float, bool>>(); // Provided to each discount application by reference
             foreach (Discount disc in discounts)
             {
                 ApplyDiscountToSKUs(ref pickListOfProducts, SKUs, disc, ref SKUDiscount);
@@ -37,23 +37,28 @@ namespace exercise.main.Utils
                 prediscountPrice = products.Where(p => p.GetSKUName() == SKU).Sum(x => x.GetBasePrice());
                 itemsOfCurrentSKU = productsOfCurrentSKU.Count();
 
-                float discountAmountOnSKU = SKUDiscount.FirstOrDefault(x => x.Key == SKU).Value;
-                float reducedPrice = 0f;
-                if (discountAmountOnSKU != 0f)
-                {
-                    reducedPrice = discountAmountOnSKU;
-                    hasAppliedDiscount = true;
-                }
-
                 if (itemsOfCurrentSKU != 0)
                 {
+                    Tuple<float, bool> discountAmountOnSKU = SKUDiscount.FirstOrDefault(x => x.Key == SKU).Value;
+                    float reducedPrice = 0f;
+                    if (discountAmountOnSKU.Item2)
+                    {
+                        reducedPrice = discountAmountOnSKU.Item1;
+                        hasAppliedDiscount = true;
+                    }
+                    else 
+                    {
+                        reducedPrice = 0f;
+                        hasAppliedDiscount = false;
+                    }
+
                     compactedProducts.Add(SKU, new Tuple<int, float, bool, float>(itemsOfCurrentSKU, reducedPrice, hasAppliedDiscount, prediscountPrice));
                 }
             }
             return compactedProducts;
         }
 
-        private static void ApplyDiscountToSKUs(ref List<IProduct> allProductsInBasket, List<string> SKUs, Discount discount, ref Dictionary<string, float> SKUDiscount)
+        private static void ApplyDiscountToSKUs(ref List<IProduct> allProductsInBasket, List<string> SKUs, Discount discount, ref Dictionary<string, Tuple<float, bool>> SKUDiscount)
         {
             List<Type> discountCondition = new List<Type>(discount.ProductSequence);
             float totalDiscountAmount = discount.DiscountPrice;
@@ -64,6 +69,7 @@ namespace exercise.main.Utils
                 foreach (string SKU in SKUs) 
                 {
                     float currentSKUAppliedDiscount = 0f;
+                    bool currentSKUDidApplyDiscount = false;
                     List<IProduct> productsOfCurrentSKU = new List<IProduct>(allProductsInBasket.Where(p => p.GetSKUName() == SKU));
                     IProduct? currentProduct = productsOfCurrentSKU.FirstOrDefault();
                     if (currentProduct != null) { 
@@ -72,11 +78,18 @@ namespace exercise.main.Utils
                         {
                             // Need to select the smallest value, we dont give customers money when they buy something!
                             float appliedDiscountValue = Math.Min(discountAmountEvenSplit, currentProduct.GetBasePrice());
+
                             foreach (Type type in matchingConditionType) // Iterate through each Type for the discount
                             {
+                                if (type == typeof(Coffee))
+                                {
+                                    appliedDiscountValue = Math.Min(totalDiscountAmount, currentProduct.GetBasePrice());
+                                }
+
                                 // Count up the remaining and applied discount
                                 totalDiscountAmount -= appliedDiscountValue;
                                 currentSKUAppliedDiscount += currentProduct.GetBasePrice() - appliedDiscountValue;
+                                currentSKUDidApplyDiscount = true;
 
                                 // Need to remove "used" allProductsInBasket, both to prevent double application and keep track of when discount is finished applying.
                                 allProductsInBasket.Remove(currentProduct); 
@@ -93,19 +106,37 @@ namespace exercise.main.Utils
                     // With mulitple discounts applied to basket we might get duplicates. Therefore need two methods to add to the dictionary
                     if (SKUDiscount.ContainsKey(SKU))
                     { // Make new float value, remove old entry from dictionary, then add the new keyvaluepair
-                        KeyValuePair<string, float> entry = SKUDiscount.Where(p => p.Key == SKU).First();
+                        KeyValuePair<string, Tuple<float, bool>> entry = SKUDiscount.Where(p => p.Key == SKU).First();
                         SKUDiscount.Remove(entry.Key);
-                        float newValue = (entry.Value + currentSKUAppliedDiscount);
-                        SKUDiscount.Add(SKU, newValue);
+                        float newValue = (entry.Value.Item1 + currentSKUAppliedDiscount);
+                        SKUDiscount.Add(SKU, new Tuple<float, bool>(newValue, currentSKUDidApplyDiscount));
                     }
                     else 
                     {
-                        SKUDiscount.Add(SKU, currentSKUAppliedDiscount);
+                        SKUDiscount.Add(SKU, new Tuple<float, bool>(currentSKUAppliedDiscount, currentSKUDidApplyDiscount));
                     }
                     // No more discount Types to apply
                     if (discountCondition.Count == 0) { break; }
                 }
             }
+        }
+
+        public static Dictionary<string, float> GetAllCoffeeSKUs() 
+        {
+            Dictionary<string, float> allIProductSKUs = Inventory.GetValidProductsInformation();
+            return allIProductSKUs.Where(sku => sku.Key.Contains("COF")).ToDictionary();
+        }
+
+        public static Dictionary<string, float> GetAllBagelSKUs()
+        {
+            Dictionary<string, float> allSKUs = Inventory.GetValidProductsInformation();
+            return allSKUs.Where(sku => sku.Key.Substring(0, 3) == "BGL").ToDictionary();
+        }
+
+        public static Dictionary<string, float> GetAllFillingSKUs()
+        {
+            Dictionary<string, float> allSKUs = Inventory.GetValidFillingInformation();
+            return allSKUs.Where(sku => sku.Key.Substring(0, 3) == "FIL").ToDictionary();
         }
     }
 }
