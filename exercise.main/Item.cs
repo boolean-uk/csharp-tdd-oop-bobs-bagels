498678dd2f6bc22bd0e5cb7281b84233996f95de
Rose Breedveld <rosebreedveld@hotmail.com>
2024-03-12 09:54:28 +0100

    added sku, price, name and variant to Item Class

--- exercise.main/Item.cs
+++ exercise.main/Item.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace exercise.main
 {
-    internal class Item
+    public class Item
     {
+        private string _sku;
+        private double _price;
+        private string _name;
+        private string _variant;
+
+        public string Sku => _sku;
+        public double Price => _price;
+        public string Name => _name;
+        public string Variant => _variant;
+
+        public Item(string sku, double price, string name, string variant)
+        {
+            _sku = sku;
+            _price = price;
+            _name = name;
+            _variant = variant;
+        }
+
     }
+
 }
