7ccb93dd8e93d70cbf032f03ff242189f0f78487
Ali Haider Khan <ali_786@live.no>
2024-08-16 11:26:16 +0200

    started

---
+++ exercise.main/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise.main
+{
+    public class Basket
+    {
+        public List<Item> item {  get; set; }  = new List<Item> { };
+
+        public int max_capasity { get; set; } = 5;
+    }
+}
