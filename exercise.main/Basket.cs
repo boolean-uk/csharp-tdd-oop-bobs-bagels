1bf2fd51a13442fdccfccee10ad9fd0c502e0f34
Julia <julia.r.lindgren@gmail.com>
2024-08-16 12:21:22 +0200

    Implement inventory class and product

---
+++ exercise.main/Basket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+using exercise;
+
+namespace exercise
+{
+    public class Basket
+    {
+        List<Product> products = new List<Product>();
+
+        public bool AddProduct(String SKU)
+        {
+
+
+            return true;
+        }
+
+        
+
+    }
+}
