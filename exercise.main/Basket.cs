f6a9f22b379bdf36bc8daf3ad057316d96552250
tvaltn <valtniemi@gmail.com>
2024-08-16 12:46:33 +0200

    test1 pass

--- exercise.main/Basket.cs
+++ exercise.main/Basket.cs
@@ -8,12 +8,13 @@ namespace exercise.main
 {
     public class Basket
     {
-        private List<Product> _products;
+        private List<Product> _products = new List<Product>();
         private static int _capacity;
 
         public bool add(string v)
         {
             Product value = BagelShop.Category[v];
+            _products.Add(value);
             return true;
         }
 
