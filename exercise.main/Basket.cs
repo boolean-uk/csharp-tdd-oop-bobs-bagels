e5916c2726f451b44b140c7dcc656242359acd40
Magnus Brandsegg <magbra00@gmail.com>
2024-08-19 15:58:47 +0200

    Added discount method. Big and clumsy code. works. Magnus Brandsegg

--- exercise.main/Basket.cs
+++ exercise.main/Basket.cs
@@ -28,7 +28,7 @@ namespace exercise.main
         }
 
         //better add method for filling and stuff
-
+        /*
         public decimal GetTotalCost()
         {
             decimal totalCost = 0M;
@@ -39,7 +39,7 @@ namespace exercise.main
             }
 
             return totalCost;      
-        }
+        }*/
 
         public bool Remove(Product removableItem)
         {
@@ -52,11 +52,107 @@ namespace exercise.main
             return false;
         }
 
+        public decimal GetTotalCost()
+        {
+            decimal totalCost = 0M;
+            //maybe take in total monaey saved for the receipt later?
+            (List<Product> noDiscountList, decimal discontShmoney) = CheckForDiscount();
+
+            totalCost += discontShmoney;
+
+            foreach (Product p in noDiscountList)
+            {
+                totalCost += p.Price;
+            }
+
+            //some random tests
+            Console.WriteLine($"total cost {totalCost}");
+            Console.WriteLine(discontShmoney);
+            Console.WriteLine(noDiscountList.Count());
+            return totalCost;
+        }
+
+        public (List<Product> noDiscountList, decimal totalDiscount) CheckForDiscount()
+        {
+            //new temp list of all items in basket
+            List<Product> product = _productList;
+
+            decimal totalDiscount = 0;
+            decimal totalMoneySaved = 0;
+            bool discountsExist = true;
+
+            //list of bagel SKU's, maybe change to get only the SKUs of bagels in the basket?
+            List<string> bagelSKUList  = new List<string> { "BGLO", "BGLP", "BGLE", "BGLS" };
+
+            //loop through tempList (while there is discountable(?) items in list)
+            while (discountsExist)
+            {
+                //loop through bagel SKUs 
+                foreach (string bagelSKU in bagelSKUList)
+                {
+                    //check if there is 12 bagels of same type
+                    if (product.Where(p => p.SKU == bagelSKU).Count() >= 12)
+                    {
+                        //price of product times 12 - discounted price
+                        totalMoneySaved += ((product.Where(p => p.SKU == bagelSKU).First().Price) * 12) - 3.99M;
+
+                        //remove 12 bagels of said type
+                        for (int i = 0; i < 12; i++)
+                        {
+                            product.Remove(product.Where(p => p.SKU == bagelSKU).First());
+                        }
+                        //add discount price to total
+                        totalDiscount += 3.99M;
+                        discount = true;
+
+                    }
+                    if (product.Where(p => p.SKU == bagelSKU).Count() >= 6)
+                    {
+                        //price of product times 6 - discounted price
+                        totalMoneySaved += ((product.Where(p => p.SKU == bagelSKU).First().Price) * 6) - 2.49M;
+
+                        //remove 6 bagels of same type
+                        for (int i = 0; i < 6; i++)
+                        {
+                            product.Remove(product.Where(p => p.SKU == bagelSKU).First());
+                        }
+                        //add discount
+                        totalDiscount += 2.49M;
+                        discount = true;
+                        
+                        continue; 
+                    }
+                }
+
+                //bagel + coffee check for only 1.25$ wow! what a deal!
+                if (product.Where(p => p.Name == "Bagel").Count() > 0 && product.Where(p => p.Name == "Coffee").Count() > 0)
+                {
+                    //price of coffee + bagel - discounted price
+                    decimal bagelPrice = product.Where(p => p.Name == "Bagel").First().Price;
+                    decimal coffeePrice = product.Where(p => p.Name == "Coffee").First().Price;
+                    totalMoneySaved = bagelPrice + coffeePrice - 1.25M;
+
+                    //remove one bagel and one coffee
+                    product.Remove(product.Where(p => p.Name == "Bagel").First());
+                    product.Remove(product.Where(p => p.Name == "Coffee").First());
+
+                    //"add discount" aka replace items with discount price
+                    totalDiscount += 1.25M;
+                    discount = true;
+
+                    continue;
+                }
+
+                //break 
+                discountsExist = false;
+            }
+
+            return (product, totalDiscount);
+        }
+
         public void PrintReceipt()
         {
-
             List<Product> uniqueProduct = _productList.Distinct().ToList();
-
 
             Console.WriteLine("    ~~~ Bob's Bagels ~~~    ");
             Console.WriteLine("");
@@ -78,14 +174,19 @@ namespace exercise.main
             Console.WriteLine($"");
             Console.WriteLine($"----------------------------");
             Console.WriteLine($"Total\t\t\t{totalCost}$");
+
+            if(discount == true)
+            {
+                Console.WriteLine($" You saved a total of {5}$ ");
+            }
             Console.WriteLine($"        Thank you        ");
             Console.WriteLine($"      for your order!      ");
             
         }
 
-        public int MaxCapacity { get; set; } = 10;
+        public int MaxCapacity { get; set; } = 20;
         
-
+        public bool discount = false;
         public List<Product> ProductList { get { return _productList; } }
 
         public int totalCost { get; set; }
--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -13,14 +13,52 @@ Product product2 = inventory.Products[1];
 Product product3 = inventory.Products[2];
 Product product4 = inventory.Products[3];
 Product product5 = inventory.Products[4];
+Product product6 = inventory.Products[5];
+Product product7 = inventory.Products[6];
+Product product8 = inventory.Products[7];
+Product product9 = inventory.Products[8];
+
+
+/*
+//discount test
+basket.Add(product1); //onion bagel * 6
 basket.Add(product1);
+basket.Add(product1);
+basket.Add(product1);
+basket.Add(product1);
+basket.Add(product1);
+basket.Add(product2); //plain bagel
+basket.Add(product2); 
+basket.Add(product2); 
+basket.Add(product2); 
+basket.Add(product2); 
+basket.Add(product2); 
+basket.Add(product3); //Everything bagel
+basket.Add(product5); // coffee black
+basket.Add(product8); // coffee latte
+*/
+
+//example order 2 from github
 basket.Add(product2);
 basket.Add(product2);
 basket.Add(product2);
-basket.Add(product4);
-basket.Add(product5);
-basket.Add(product5);
-basket.Add(product5);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
+basket.Add(product2);
 
 
-basket.PrintReceipt();
+
+basket.GetTotalCost();
+
+
+//basket.PrintReceipt();
