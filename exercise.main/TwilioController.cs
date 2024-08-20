5b170ae2e1c823ca392570ee2cf9f85fd7d07ad1
Muzea001 <muat0072@gmail.com>
2024-08-20 14:53:23 +0200

    Create TwilioController.cs
    added controller

---
+++ exercise.main/TwilioController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio.AspNet.Common;
+using Twilio.TwiML;
+using Twilio.TwiML.Fax;
+using exercise.main;
+
+
+namespace exercise.main
+{
+    [Route("api/TwilioController")]
+    [ApiController]
+    public class TwilioController
+    {
+
+        private Basket _basket;
+
+        public TwilioController(Basket basket)
+        {
+            _basket = basket;
+        }
+        [HttpPost("orderBySms")]
+        public IActionResult OrderBySms([FromForm] SmsRequest incomingMessage)
+        {
+
+           
+            var sentFrom = incomingMessage.From;
+            var body = incomingMessage.Body;
+            var response = _basket.receiveSmsOrder(sentFrom, body);
+
+            var messagingResponse = new MessagingResponse();
+            messagingResponse.Message(response);
+
+            return new ContentResult
+            {
+                Content = messagingResponse.ToString(),
+                ContentType = "application/xml"
+            };
+        }
+
+    }
+}
