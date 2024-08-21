5c4b13f2a83d65b70a1042e1e96de968f0f7d891
Jonas <jonashalvorsen2013@live.no>
2024-08-21 11:05:38 +0200

    Exercise1&3-work-in-progress

--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -1,14 +1,22 @@
 ﻿using exercise.main;
 
 BobsBagelStore store = new BobsBagelStore();
-Basket basket = new Basket(5);
+Basket basket = new Basket(30);
 Item item1 = new Item("BGLE", 0.39f, "Bagel", "Everything");
-Item item2 = new Item("BGLS", 0.49f, "Bagel", "Sesame");
+Item item2 = new Item("BGLP", 0.39f, "Bagel", "Plain");
 Item item3 = new Item("COFB", 0.99f, "Coffee", "Black");
 Item item4 = new Item("FILX", 0.12f, "Filling", "Cream Cheese");
 
-basket.AddItem(item1);
-basket.AddItem(item2);
+for (int  i = 0; i < 7; i++)
+{
+    basket.AddItem(item1);
+}
+
+for (int i = 0; i < 15; i++)
+{
+    basket.AddItem(item2);
+}
+
 basket.AddItem(item3);
 basket.AddItem(item3);
 basket.AddItem(item4);
--- exercise.main/Receipt.cs
+++ exercise.main/Receipt.cs
@@ -16,14 +16,19 @@ namespace exercise.main
         public Receipt(Basket basket)
         {
             _items = basket.Items;
-            _discounts.Add(new Discount("BGLO", 6, 2.49f));
-            _discounts.Add(new Discount("BGLP", 12, 3.99f));
-            _discounts.Add(new Discount("BGLE", 6, 2.49f));
-            _discounts.Add(new Discount("COFB", 1, 1.25f));
+            _discounts = new List<Discount>()
+            {
+                new Discount("BGLO", 6, 2.49f),
+                new Discount("BGLP", 12, 3.99f),
+                new Discount("BGLE", 6, 2.49f),
+                new Discount("COFB", 1, 1.25f)
+            };
         }
 
         public string PrintReceipt()
         {
+            Dictionary<Item, (int, float)> discountedItems = CalculateDiscounts(new Dictionary<Item, int>(_items)); 
+
             float total = 0;
             float itemTotal = 0;
             StringBuilder message = new StringBuilder();
@@ -35,15 +40,15 @@ namespace exercise.main
                 return message.ToString();
             }
 
-            message.Append("    ~~~ Bob's Bagels ~~~\n\n");
-            message.Append($"    {DateTime.Now}\n\n");
-            message.Append("----------------------------\n\n");
+            message.Append("       ~~~ Bob's Bagels ~~~\n\n");
+            message.Append($"       {DateTime.Now}\n\n");
+            message.Append("---------------------------------\n\n");
 
             foreach (Item item in _items.Keys)
             {
                 if (_items[item] > 0)
                 {
-                    message.Append($"{item.Name}\t\t");
+                    message.Append($"{item.Variant} {item.Name}".PadRight(23));
                     message.Append($"{_items[item]}".PadRight(5));
                     itemTotal = item.Price * _items[item];
                     message.Append($"£{itemTotal}\n");
@@ -51,14 +56,87 @@ namespace exercise.main
                 }
             }
 
-            message.Append("\n----------------------------\n");
-            message.Append($"Total\t\t".PadRight(12));
+            foreach (Item discountedItem in discountedItems.Keys) 
+            {
+                if (discountedItems[discountedItem].Item1 > 0)
+                {
+                    message.Append($"{discountedItem.Variant} {discountedItem.Name}".PadRight(23));
+                    message.Append($"{discountedItems[discountedItem].Item1}".PadRight(5));
+                    itemTotal = discountedItems[discountedItem].Item2;
+                    message.Append($"£{itemTotal}\n");
+                    total += itemTotal;
+                }
+            }
+            message.Append("\n---------------------------------\n");
+            message.Append($"Total\t\t".PadRight(18));
             message.Append($"£{total}\n\n");
-            message.Append("\t  Thank you\n\tfor you order!\n");
+            message.Append("\t    Thank you\n\t  for you order!\n");
 
             Console.Write(message.ToString());
 
             return message.ToString();
+        }
+
+        public Dictionary<Item, (int, float)> CalculateDiscounts(Dictionary<Item, int> items)
+        {
+            Dictionary <Item, (int, float)> discountedItems = new Dictionary<Item, (int, float)>();
+            foreach (Discount discount in _discounts)
+            {
+                if (items.Keys.Where(x => x.SKU == discount.DiscountItemSKU).ToList().Count() > 0)
+                {
+                    Item foundItem = items.Keys.Where(x => x.SKU == discount.DiscountItemSKU).ToList()[0];
+
+                    if (foundItem.SKU == "COFB")
+                    {
+                        if (items.Keys.Where(x => x.Name == "Bagel").ToList().Count() > 0)
+                        {
+                            Item foundBagel = items.Keys.Where(x => x.Name == "Bagel").ToList()[0];
+
+                            while (items[foundItem] >= (discount.ItemsHit + discount.NumberOfRequiredItems) && items[foundBagel] >= (discount.ItemsHit + discount.NumberOfRequiredItems))
+                            {
+                                discount.NumberOfDiscountsHit++;
+                                discount.ItemsHit += discount.NumberOfRequiredItems;
+                                items[foundItem] -= discount.NumberOfRequiredItems;
+                                items[foundBagel] -= discount.NumberOfRequiredItems;
+                            }
+
+                            if (discount.NumberOfDiscountsHit > 0)
+                            {
+                                if (discountedItems.ContainsKey(foundItem))
+                                {
+                                    discountedItems[foundItem] = (discount.ItemsHit, discount.DiscountPrice * discount.NumberOfDiscountsHit);
+                                }
+                                else
+                                {
+                                    discountedItems.Add(foundItem, (discount.ItemsHit, discount.DiscountPrice * discount.NumberOfDiscountsHit));
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        while (items[foundItem] >= (discount.ItemsHit + discount.NumberOfRequiredItems))
+                        {
+                            discount.NumberOfDiscountsHit++;
+                            discount.ItemsHit += discount.NumberOfRequiredItems;
+                            items[foundItem] -= discount.NumberOfRequiredItems;
+                        }
+
+                        if (discount.NumberOfDiscountsHit > 0)
+                        {
+                            if (discountedItems.ContainsKey(foundItem))
+                            {
+                                discountedItems[foundItem] = (discount.ItemsHit, discount.DiscountPrice * discount.NumberOfDiscountsHit);
+                            }
+                            else
+                            {
+                                discountedItems.Add(foundItem, (discount.ItemsHit, discount.DiscountPrice * discount.NumberOfDiscountsHit));
+                            }
+                        }
+                    }
+                }
+            }
+            return discountedItems;
         }
 
         public Dictionary<Item, int> Items { get { return _items; } }
