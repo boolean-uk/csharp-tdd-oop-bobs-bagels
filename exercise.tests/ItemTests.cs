e688f7c270c63093230b29e7ceec2a5ffa6e6aed
John Ferdie Abueg <john.abueg@gmail.com>
2024-08-22 14:22:15 +0200

    Forgot to update a test in ItemTests

--- exercise.tests/ItemTests.cs
+++ exercise.tests/ItemTests.cs
@@ -24,47 +24,6 @@ public class ItemTests
 
         // Verify
         Assert.IsTrue(onionBagel.Price == 2.49);
-
-        //Customer customer = new Customer("customer", false);
-        //ShopInventory shopInventory = new ShopInventory();
-        //customer.Basket.Capacity = 23;
-
-        //// 2 onion Bagel
-        //Bagel onionBagel = shopInventory.GetBagelBySkuID("BGLO");
-        //customer.AddItemToBasket(onionBagel);
-        //customer.AddItemToBasket(onionBagel);
-
-        //// 12 Plain Bagel
-        //Bagel plainBagel = shopInventory.GetBagelBySkuID("BGLP");
-        //for (int i = 0; i < 12; i++)
-        //{
-        //    customer.AddItemToBasket(plainBagel);
-        //}
-
-        //// 6 Everything Bagel
-        //Bagel everythingBagel = shopInventory.GetBagelBySkuID("BGLE");
-        //for (int i = 0; i < 6; i++)
-        //{
-        //    customer.AddItemToBasket(everythingBagel);
-        //}
-
-        //// 3 Black Coffee
-        //Coffee coffee = shopInventory.GetCoffeeBySkuID("COFB");
-        //customer.AddItemToBasket(coffee);
-        //customer.AddItemToBasket(coffee);
-        //customer.AddItemToBasket(coffee);
-
-        //string shopName = "Bob's Bagels";
-
-        //Receipt customerReceipt = customer.Checkout(shopName);
-
-        //Assert.IsTrue(customerReceipt != null);
-        //Assert.IsTrue(customerReceipt?.BoughtItems.Count() == 4);
-        //Assert.IsTrue(customer.Basket.Capacity == 0);
-        //Assert.IsTrue(onionBagel.Quantity == 2);
-        //Assert.IsTrue(plainBagel.Quantity == 12);
-        //Assert.IsTrue(everythingBagel.Quantity == 6);
-        //Assert.IsTrue(coffee.Quantity == 3);
     }
 
     [Test]
@@ -184,6 +143,6 @@ public class ItemTests
         }
 
         // Verify
-        Assert.IsTrue(plainBagel.MoneySaved == -0.69);
+        Assert.IsTrue(plainBagel.MoneySaved == 0.69);
     }
 }
