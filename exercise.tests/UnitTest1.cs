1fb015bdce6c415e10982e23ec8c74c1e109708f
George <george.alexander.saav@gmail.com>
2024-08-23 13:21:14 +0200

    Made a better test for extension 1

--- exercise.tests/UnitTest1.cs
+++ exercise.tests/UnitTest1.cs
@@ -305,6 +305,9 @@ public class Tests
     [Test]
     public void BagelDiscountTest()
     {
+        // Testing for 12 discounted Bagels, and 1 not discounted bagel
+        double expected = 41.56;
+
         Basket basket = new Basket();
         basket.Cap = 20;
 
@@ -312,85 +315,43 @@ public class Tests
         Inventory.BagelStock = 20;
         Inventory.CoffeeStock = 20;
 
-        double expected = 4.48;
+        List<Item> preselection = basket.MakeNew();
+        List<Filling> fillings = new List<Filling>();
+        Filling filling1 = ChosenItem.ChooseFillings("FILE", 0.12, "Filling", "Egg");
+        fillings.Add(filling1);
+        Bagel bagel1 = ChosenItem.MakeBagel("BGLE", 0.49, "Bagel", "Everything", "");
+        bagel1 = ChosenItem.AddFillings(bagel1, fillings);
+        Coffee cup1 = new Coffee("COFB", 0.99, "Coffee", "Black");
+        Coffee cup2 = new Coffee("COFB", 0.99, "Coffee", "Black");
+        Coffee cup3 = new Coffee("COFB", 0.99, "Coffee", "Black");
 
-        List<Item> preselection3 = basket.MakeNew();
-        Bagel bagel3 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection3.Add(bagel3);
-        List<Item> selectedItems3 = basket.AddToSelection(preselection3);
-        basket.AddToBasket(3, selectedItems3);
+        Bagel bagel2 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
+        List<Item> preselection2 = basket.MakeNew();
+        preselection2.Add(bagel2);
 
-        List<Item> preselection4 = basket.MakeNew();
-        Bagel bagel4 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection4.Add(bagel4);
-        List<Item> selectedItems4 = basket.AddToSelection(preselection4);
-        basket.AddToBasket(4, selectedItems4);
+        preselection.Add(cup1);
+        preselection.Add(cup2);
+        preselection.Add(cup3);
+        preselection.Add(bagel1);
 
-        List<Item> preselection5 = basket.MakeNew();
-        Bagel bagel5 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection5.Add(bagel5);
-        List<Item> selectedItems5 = basket.AddToSelection(preselection5);
-        basket.AddToBasket(5, selectedItems5);
+        preselection.Add(filling1);
 
-        List<Item> preselection6 = basket.MakeNew();
-        Bagel bagel6 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection6.Add(bagel6);
-        List<Item> selectedItems6 = basket.AddToSelection(preselection6);
-        basket.AddToBasket(6, selectedItems6);
+        basket.AddToBasket(1, preselection);
+        basket.AddToBasket(2, preselection);
+        basket.AddToBasket(3, preselection);
+        basket.AddToBasket(4, preselection);
+        basket.AddToBasket(5, preselection);
+        basket.AddToBasket(6, preselection);
 
-        List<Item> preselection7 = basket.MakeNew();
-        Bagel bagel7 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection7.Add(bagel7);
-        List<Item> selectedItems7 = basket.AddToSelection(preselection7);
-        basket.AddToBasket(7, selectedItems7);
+        basket.AddToBasket(7, preselection);
+        basket.AddToBasket(8, preselection);
+        basket.AddToBasket(9, preselection);
+        basket.AddToBasket(10, preselection);
+        basket.AddToBasket(11, preselection);
+        basket.AddToBasket(12, preselection);
+        basket.AddToBasket(13, preselection2);
 
-        List<Item> preselection8 = basket.MakeNew();
-        Bagel bagel8 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection8.Add(bagel8);
-        List<Item> selectedItems8 = basket.AddToSelection(preselection8);
-        basket.AddToBasket(8, selectedItems8);
-
-        List<Item> preselection9 = basket.MakeNew();
-        Bagel bagel9 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection9.Add(bagel9);
-        List<Item> selectedItems9 = basket.AddToSelection(preselection9);
-        basket.AddToBasket(9, selectedItems9);
-
-        List<Item> preselection10 = basket.MakeNew();
-        Bagel bagel10 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection10.Add(bagel10);
-        List<Item> selectedItems10 = basket.AddToSelection(preselection10);
-        basket.AddToBasket(10, selectedItems10);
-
-        List<Item> preselection11 = basket.MakeNew();
-        Bagel bagel11 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection11.Add(bagel11);
-        List<Item> selectedItems11 = basket.AddToSelection(preselection11);
-        basket.AddToBasket(11, selectedItems11);
-
-        List<Item> preselection12 = basket.MakeNew();
-        Bagel bagel12 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection12.Add(bagel12);
-        List<Item> selectedItems12 = basket.AddToSelection(preselection12);
-        basket.AddToBasket(12, selectedItems12);
-
-        List<Item> preselection13 = basket.MakeNew();
-        Bagel bagel13 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection13.Add(bagel13);
-        List<Item> selectedItems13 = basket.AddToSelection(preselection13);
-        basket.AddToBasket(13, selectedItems13);
-
-        List<Item> preselection14 = basket.MakeNew();
-        Bagel bagel14 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection14.Add(bagel14);
-        List<Item> selectedItems14 = basket.AddToSelection(preselection14);
-        basket.AddToBasket(14, selectedItems14);
-
-        List<Item> preselection15 = basket.MakeNew();
-        Bagel bagel15 = ChosenItem.MakeBagel("BGLO", 0.49, "Bagel", "Onion", "");
-        preselection15.Add(bagel15);
-        List<Item> selectedItems15 = basket.AddToSelection(preselection15);
-        basket.AddToBasket(15, selectedItems15);
+        //Console.WriteLine(basket.PrintBasket());
 
         double result = basket.BasketTotal();
 
