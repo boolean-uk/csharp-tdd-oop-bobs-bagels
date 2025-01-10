9db7cdd8ef7d28bcfb161a94b870825da8ad04ca
Noah Ekse <noah.ekse@no.experis.com>
2025-01-10 15:01:40 +0100

    all tests

--- exercise.tests/Tests.cs
+++ exercise.tests/Tests.cs
@@ -81,4 +81,146 @@ public class Tests
         // assert
         Assert.That(_basket.GetItems().Count, Is.EqualTo(2));
     }
+
+    [Test]
+    public void TestChangeCapacity()
+    {
+        // arrange
+
+
+        // act
+        
+
+        // assert
+        
+    }
+
+    [Test]
+    public void TestBasketFull()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+
+    [Test]
+    public void TestCheckIfItemExistsInBasket()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+
+    [Test]
+    public void TestCheckTotalCostOfItemsInBasket()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+    [Test]
+    public void TestCheckCostOfBagel()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+
+    [Test]
+    public void TestChooseFillings()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+
+    [Test]
+    public void TestCheckFillingCost()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+
+    [Test]
+    public void TestAddCoffeeToBasket()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+
+    [Test]
+    public void TestCheckCoffeeCost()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+
+    [Test]
+    public void TestAddPromotionToProduct()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
+
+    [Test]
+    public void TestCheckStockOfProduct()
+    {
+        // arrange
+
+
+        // act
+
+
+        // assert
+
+    }
 }
