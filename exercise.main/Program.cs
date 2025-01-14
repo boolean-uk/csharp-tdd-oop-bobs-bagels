481b0368400db4fc0ec15f64d7991d047b30cf74
Jostein Lind Aanby <jostein.aanby@no.experis.com>
2025-01-14 14:33:44 +0100

    added reciept

--- exercise.main/Program.cs
+++ exercise.main/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using exercise.main.Products;
+using exercise.main.UI;
+
+List<Product> _products = new List<Product>();
+
+for (int i = 0; i < 12; i++)
+{
+    _products.Add(new Bagel("BGLP", 0.39, "Bagel", "Plain"));
+}
+
+for (int i = 0; i < 6; i++)
+{
+    _products.Add(new Bagel("BGLE", 0.49, "Bagel", "Everything"));
+}
+
+for (int i = 0; i < 2; i++)
+{
+    _products.Add(new Bagel("BGLO", 0.49, "Bagel", "Onion"));
+}
+
+
+for (int i = 0; i < 3; i++)
+{
+    _products.Add(new Coffee("COFB", 0.99, "Coffee", "Black"));
+}
+Reciept rec = new Reciept(_products);
+Console.WriteLine(rec.ToString());
--- exercise.main/UI/Reciept.cs
+++ exercise.main/UI/Reciept.cs
@@ -3,12 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using exercise.main.Inventories;
+using exercise.main.Products;
 
 namespace exercise.main.UI
 {
     public class Reciept
     {
 
+        private List<Product> _boughtProducts;
 
+        public Reciept(List<Product> boughtProducts)
+        {
+            this._boughtProducts = boughtProducts;
+
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("~~~ Bob's Bagels ~~~");
+            sb.AppendLine();
+            sb.AppendLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            sb.AppendLine();
+            sb.AppendLine(new string('-', 28));
+
+            // Add items
+            var groupedProducts = _boughtProducts
+            .GroupBy(product => product.Sku)
+            .Select(g => new
+            {
+                Sku = g.Key,
+                Variant = g.First().Variant, // Assuming all products in a group have the same variant
+                Count = g.Count(),
+                TotalPrice = Inventory.Instance.GetPrice(g.First()) * g.Count()
+            });
+
+            foreach (var item in groupedProducts)
+            {
+                sb.AppendLine($"{item.Variant} Bagel   {item.Count}   £{item.TotalPrice:F2}");
+            }
+
+            // Add separator
+            sb.AppendLine(new string('-', 28));
+
+            // Add total
+            sb.AppendLine("Total                 £10.43");
+            sb.AppendLine();
+
+            // Add footer
+            sb.AppendLine("        Thank you");
+            sb.AppendLine("      for your order!");
+
+            return sb.ToString();
+        }
     }
 }
